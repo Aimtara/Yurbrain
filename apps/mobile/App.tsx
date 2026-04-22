e5d2d8593e444c0fbd135e3d3399e5b00a6179e2
Aimtara <223397310+Aimtara@users.noreply.github.com>
2026-04-21 21:32:29 -0400

    Integrate NhostProvider into App component

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,1 +1,17 @@
+import { NhostProvider, NhostClient } from '@nhost/react';
+
+const nhost = new NhostClient({
+  authUrl: process.env.EXPO_PUBLIC_NHOST_AUTH_URL,
+  graphqlUrl: process.env.EXPO_PUBLIC_NHOST_GRAPHQL_URL,
+  functionsUrl: process.env.EXPO_PUBLIC_NHOST_FUNCTIONS_URL
+});
+
+export default function App() {
+  return (
+    <NhostProvider nhost={nhost}>
+       {./src/App}
+    </NhostProvider>
+  );
+}
+
 export { default } from "./src/App";
